9c616decf5e0d0b6cbc15473af2704116081b0c3
morshedul-antor <antor.morshedul@gmail.com>
2024-02-15 18:04:57 +0600

    add: valid palindrome solution

---
+++ 125.ValidPalindrome/solution.py
@@ -0,0 +1,17 @@
+# Time-Complexity: O(n)
+# Space-Complexity: O(n)
+
+def valid_palindrome(s):
+    main_str = s.translate(str.maketrans(
+        "", "", " ,:!@#$%^&*()-_=+[]{};:'\"<>,.?/\\|`~")).lower()
+    reversed_str = ""
+
+    for i in main_str:
+        reversed_str = i + reversed_str
+
+    return reversed_str == main_str
+
+
+print(valid_palindrome(s="A man, a plan, a canal: Panama"))
+print(valid_palindrome(s="race a car"))
+print(valid_palindrome(s="ab@a"))
