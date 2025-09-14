42ba25b9f487125d79847b5da313eeaf5aeacf6a
morshedul-antor <antor.morshedul@gmail.com>
2025-09-14 17:05:50 +0600

    feat: better hashmap solution add for reverse string

---
+++ 344.ReverseString/solution_brute_force.py
@@ -0,0 +1,24 @@
+# Time-Complexity: O(n)
+# Space-Complexity: O(n)
+
+
+def reverse_string(s):
+    reverse = []
+
+    for i in range(len(s)-1, -1, -1):
+        reverse.append(s[i])
+
+    return reverse
+
+
+def run_tests():
+    assert reverse_string(
+        s=["h", "e", "l", "l", "o"]) == ["o", "l", "l", "e", "h"]
+    assert reverse_string(
+        s=["H", "a", "n", "n", "a", "h"]) == ["h", "a", "n", "n", "a", "H"]
+    assert reverse_string(s=["a", "c"]) == ["c", "a"]
+    print("All test cases passed...")
+
+
+if __name__ == "__main__":
+    run_tests()
