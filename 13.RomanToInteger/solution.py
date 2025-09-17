8db1f395fa5ad66fa7df0cb0f586bd885a8dfc33
morshedul-antor <antor.morshedul@gmail.com>
2025-09-17 14:54:38 +0600

    modify: code

--- 13.RomanToInteger/solution.py
+++ 13.RomanToInteger/solution.py
@@ -2,23 +2,29 @@
 # Space-Complexity: O(1)
 
 
-def romanToInt(s):
+def roman_to_integer(s):
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
     result = 0
     prev_value = 0
 
     for i in s:
-        value = romans[i]
-        result += value
+        result += romans[i]
 
-        if value > prev_value:
+        if romans[i] > prev_value:
             result -= 2 * prev_value
-        prev_value = value
+
+        prev_value = romans[i]
 
     return result
 
 
-print(romanToInt(s="III"))
-print(romanToInt(s="LVIII"))
-print(romanToInt(s="MCMXCIV"))
+def run_tests():
+    assert roman_to_integer(s="III") == 3
+    assert roman_to_integer(s="LVIII") == 58
+    assert roman_to_integer(s="MCMXCIV") == 1994
+    print("All test cases passed...")
+
+
+if __name__ == "__main__":
+    run_tests()
