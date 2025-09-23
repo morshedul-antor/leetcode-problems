c8c91eb7920798e87b8dfe80c6d6c049e8f41bb3
morshedul-antor <antor.morshedul@gmail.com>
2025-09-23 15:30:07 +0600

    feat: brute force solution add for number complement

---
+++ 476.NumberComplement/solution_brute_force.py
@@ -0,0 +1,25 @@
+# Time-Complexity: O(log n)
+# Space-Complexity: O(log n)
+
+
+def number_complement(num: int) -> int:
+    binary = bin(num)[2:]
+    value = ""
+
+    for num in binary:
+        if num == "0":
+            value += "1"
+        else:
+            value += "0"
+
+    return int(value, 2)
+
+
+def run_tests():
+    assert number_complement(num=5) == 2
+    assert number_complement(num=1) == 0
+    print("All test cases passed...")
+
+
+if __name__ == "__main__":
+    run_tests()
