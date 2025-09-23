e96e21c154d24562a703d57dff4a481b7b509cb5
morshedul-antor <antor.morshedul@gmail.com>
2025-09-23 12:57:06 +0600

    feat: optimal solution add for word pattern

---
+++ 290.WordPattern/solution.py
@@ -0,0 +1,37 @@
+# Time-Complexity: O(n+m)
+# Space-Complexity: O(m)
+
+
+def word_pattern(pattern: str, s: str) -> bool:
+    s = s.split()
+    seen = {}
+    word = set()
+
+    if len(pattern) != len(s):
+        return False
+
+    for i, val in enumerate(pattern):
+        if val in seen:
+            if seen[val] != s[i]:
+                continue
+            return False
+        else:
+            if s[i] in word:
+                return False
+
+            seen[val] = s[i]
+            word.add(s[i])
+
+    return True
+
+
+def run_tests():
+    assert word_pattern(pattern="abba", s="dog cat cat dog") == True
+    assert word_pattern(pattern="abba", s="dog cat cat fish") == False
+    assert word_pattern(pattern="aaaa", s="dog cat cat dog") == False
+    assert word_pattern(pattern="abba", s="dog dog dog dog") == False
+    print("All test cases passed...")
+
+
+if __name__ == "__main__":
+    run_tests()
