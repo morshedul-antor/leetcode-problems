bbcdbff2da36404c45556aff1042482be1c392e1
morshedul-antor <antor.morshedul@gmail.com>
2025-09-24 16:23:29 +0600

    feat: brute force solution add for power of two

---
+++ 231.PowerOfTwo/solution_brute_force.py
@@ -0,0 +1,24 @@
+# Time-Complexity: O(log n)
+# Space-Complexity: O(1)
+
+
+def power_of_two(n):
+    if n < 1:
+        return False
+
+    i = 1
+    while i < n:
+        i *= 2
+
+    return i == n
+
+
+def run_tests():
+    assert power_of_two(n=1) == True
+    assert power_of_two(n=16) == True
+    assert power_of_two(n=3) == False
+    print("All test cases passed...")
+
+
+if __name__ == "__main__":
+    run_tests()
