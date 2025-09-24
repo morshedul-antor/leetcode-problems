fb6d7d3e5ff05859e7fe39ec55514036fad1ea0f
morshedul-antor <antor.morshedul@gmail.com>
2025-09-24 16:22:53 +0600

    feat: optimal solution add for number of 1 bits

---
+++ 191.NumberOf1Bits/solution_optimal_brian_kernighan.py
@@ -0,0 +1,23 @@
+# Time-Complexity: O(k) k = number of 1s in n.
+# Space-Complexity: O(1)
+
+
+def number_of_bits(n: int) -> int:
+    count = 0
+
+    while n:
+        n &= n - 1
+        count += 1
+
+    return count
+
+
+def run_tests():
+    assert number_of_bits(n=11) == 3
+    assert number_of_bits(n=128) == 1
+    assert number_of_bits(n=2147483645) == 30
+    print("All test cases passed...")
+
+
+if __name__ == "__main__":
+    run_tests()
