1fef8c3753833a73410b8002ef50fa0d75e623b9
morshedul-antor <antor.morshedul@gmail.com>
2025-09-25 13:56:13 +0600

    feat: brute force solution add for reverse word in a string

---
+++ 557.ReverseWordsInString/solution_brute_force.py
@@ -0,0 +1,28 @@
+# Time-Complexity: O(n^2)
+# Space-Complexity: O(n)
+
+
+def reverse_word_in_string(s):
+    s = s.split()
+    res = []
+
+    for words in s:
+        reverse = ""
+
+        for char in words:
+            reverse = char + reverse
+        res.append(reverse)
+
+    return " ".join(res)
+
+
+def run_tests():
+    assert reverse_word_in_string(
+        s="Let's take LeetCode contest") == "s'teL ekat edoCteeL tsetnoc"
+    assert reverse_word_in_string(s="Mr Ding") == "rM gniD"
+    assert reverse_word_in_string(s="Race Car") == "ecaR raC"
+    print("All test cases passed...")
+
+
+if __name__ == "__main__":
+    run_tests()
